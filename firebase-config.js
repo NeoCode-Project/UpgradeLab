c1998475076648e5c3e42f7cbdd42e0bf1576a62
NeoCode-Project <tel95651@gmail.com>
2026-07-27 19:47:03 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBRYQlUEKzI9jRtoBHlfHD6lCEmpeNzzWw",
-  authDomain: "upgradelab.firebaseapp.com",
-  projectId: "upgradelab",
-  storageBucket: "upgradelab.firebasestorage.app",
-  messagingSenderId: "762251871566",
-  appId: "1:762251871566:web:5533b6fff33e1b0a6f4a1f",
-  measurementId: "G-EW57JJDVNG"
-};
